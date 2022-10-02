da26d7a7d9c732d58656926c3bbf783e22c010b9
krlvn <krlvn39@gmail.com>
2022-10-02 16:20:41 +0200

    Функция команды /start

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -7,11 +7,19 @@ from aiogram.filters import Command
 # from aiogram.fsm.context import FSMContext
 # from aiogram.fsm.state import State, StatesGroup
 
+from bot.db.requests import *
 from bot.languages import TEXT
 
 
 router = Router()
 
 @router.message(Command(commands=['start','en','fr','es','ru']))
-async def cmd_rules(message: Message):
-    await message.answer('<b>Hello bot!</b>')
+async def cmd_start(message: Message):
+    uid = message.from_user.id
+    user = await get_or_create_user(uid, message.from_user.language_code)
+
+    if user.language not in ['en','fr','es','ru']:
+        user.language = 'en'
+
+    # TODO: Отправляем стикер и текст или фото с подписью.
+    await message.answer(TEXT[user.language.upper()][1])
