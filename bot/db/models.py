40fa02dd0cd47a908e571a3ffcb2b5dec24783d4
krlvn <krlvn39@gmail.com>
2022-10-02 14:02:53 +0200

    Модель User

---
+++ bot/db/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, BigInteger, DateTime
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.sql import functions
+
+Base = declarative_base()
+metadata = Base.metadata
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column('id', BigInteger, unique=True, nullable=False, primary_key=True,
+                comment='ID of telegram user')
+    language = Column('language', String(length=5),
+                comment='Selected language')
+    created_at = Column('created_at', DateTime(timezone=True), server_default=functions.now())
+
+    def __repr__(self):
+        return f'<User:{self.id}>'
