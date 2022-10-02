ad8ef332eca0c63bb22c2464cc615146a0ea2ac1
krlvn <krlvn39@gmail.com>
2022-10-03 00:35:20 +0200

    Добавил отступы между строками.

--- bot/languages.py
+++ bot/languages.py
@@ -1,18 +1,18 @@
 TEXT = {
    'EN' : {
-       1 : 'Hello! \nI\'m bot-simulator of the "Magic 8-Ball", that is  used for fortune-telling or seeking advice. \nAsk me a question and get the answer.',
+       1 : 'Hello! \n\nI\'m bot-simulator of the "Magic 8-Ball", that is  used for fortune-telling or seeking advice. \n\nAsk me a question and get the answer.',
        2 : 'ONCE AGAIN',
    },
    'FR' : {
-       1 : 'Bonjour! \nJe suis un simulateur de boule magique "Magic 8-Ball". La boule est censée prédire l\'avenir et répondre à n\'importe quelle question posée. \nPosez-moi la question et obtenez la réponse.',
+       1 : 'Bonjour! \n\nJe suis un simulateur de boule magique "Magic 8-Ball". La boule est censée prédire l\'avenir et répondre à n\'importe quelle question posée. \n\nPosez-moi la question et obtenez la réponse.',
        2 : 'ENCORE UNE FOIS',
    },
    'ES' : {
-       1 : 'Hola! \nSoy un robot simulador de bola mágica "Magic 8-Ball" que se utiliza para la adivinación o la búsqueda de consejos. \nHazme una pregunta y consigue la respuesta.',
+       1 : 'Hola! \n\nSoy un robot simulador de bola mágica "Magic 8-Ball" que se utiliza para la adivinación o la búsqueda de consejos. \n\nHazme una pregunta y consigue la respuesta.',
        2 : 'UNA VEZ MÁS',
    },
    'RU' : {
-       1 : 'Привет! \nЯ бот-симулятор магического шара "Magic 8-Ball", который используется для гаданий или поиска советов. \nЗадай мне вопрос и получи ответ.',
+       1 : 'Привет! \n\nЯ бот-симулятор магического шара "Magic 8-Ball", который используется для гаданий или поиска советов. \n\nЗадай мне вопрос и получи ответ.',
        2 : 'ЕЩЁ ПОПЫТКА',
    }
 }
