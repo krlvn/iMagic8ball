d562c253030952effebd9351bc04ec91b485eac8
krlvn <krlvn39@gmail.com>
2022-10-02 16:19:41 +0200

    Убрал текст "Неизвестаня команда"

--- bot/languages.py
+++ bot/languages.py
@@ -2,21 +2,17 @@ TEXT = {
    'EN' : {
        1 : 'Hello! \nI\'m bot-simulator of the "Magic 8-Ball", that is  used for fortune-telling or seeking advice. \nAsk me a question and get the answer.',
        2 : 'ONCE AGAIN',
-       3 : 'Unrecognized command.'
    },
    'FR' : {
        1 : 'Bonjour! \nJe suis un simulateur de boule magique "Magic 8-Ball". La boule est censée prédire l\'avenir et répondre à n\'importe quelle question posée. \nPosez-moi la question et obtenez la réponse.',
        2 : 'ENCORE UNE FOIS',
-       3 : 'Unrecognized command.'
    },
    'ES' : {
        1 : 'Hola! \nSoy un robot simulador de bola mágica "Magic 8-Ball" que se utiliza para la adivinación o la búsqueda de consejos. \nHazme una pregunta y consigue la respuesta.',
        2 : 'UNA VEZ MÁS',
-       3 : 'Unrecognized command.'
    },
    'RU' : {
        1 : 'Привет! \nЯ бот-симулятор магического шара "Magic 8-Ball", который используется для гаданий или поиска советов. \nЗадай мне вопрос и получи ответ.',
        2 : 'ЕЩЁ ПОПЫТКА',
-       3 : 'Нераспознанная комманда.'
    }
 }
